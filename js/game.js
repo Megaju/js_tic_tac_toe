7d10f519d4653c4924a11af695ee8672cc5a78b2
Megaju <julien_malle@outlook.fr>
2016-07-27 16:20:54 +0200

    possible de jouer, mais pas de score

--- js/game.js
+++ js/game.js
@@ -1,3 +1,20 @@
+// gestion du tour par tour
+var tour = "playerA";
+
 $("#board-game td").click(function () {
-    $(this).addClass("playerA")
+    $(this).addClass(tour);
+    if (tour === "playerA") {
+        tour = "playerB";
+        $("#player1").removeClass("playerActif");
+        $("#player2").addClass("playerActif");
+    } else {
+        tour = "playerA";
+        $("#player2").removeClass("playerActif");
+        $("#player1").addClass("playerActif");
+    }
 });
+
+// différentes conditions de victoire
+// 123 | 456 | 789 | 147 | 258 | 369 | 159 | 357
+// si un joueur possède une de ces 3 combinaisons, il gagne
+
