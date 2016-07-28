deac1df0f3888c2a4901721c6946a2c210170a94
Megaju <julien_malle@outlook.fr>
2016-07-28 10:57:18 +0200

    système de victoire non finalisé

--- js/game.js
+++ js/game.js
@@ -1,5 +1,8 @@
 // gestion du tour par tour
 var tour = "playerA";
+// les cases prise par les joueurs
+var casePlayerA = [0];
+var casePlayerB = [0];
 
 $("#board-game td").click(function () {
     $(this).addClass(tour);
@@ -14,7 +17,59 @@ $("#board-game td").click(function () {
     }
 });
 
+$("#1").click(function () {
+    if (tour === "playerA") {
+        casePlayerA.push(1);
+        testVicyory();
+    }
+});
+$("#2").click(function () {
+    if (tour === "playerA") {
+        casePlayerA.push(2);
+        testVicyory();
+    }
+});
+$("#3").click(function () {
+    if (tour === "playerA") {
+        casePlayerA.push(3);
+        testVicyory();
+    }
+});
+
 // différentes conditions de victoire
 // 123 | 456 | 789 | 147 | 258 | 369 | 159 | 357
 // si un joueur possède une de ces 3 combinaisons, il gagne
+function playerAWin() {
+    alert("Player A win ! ! !")
+}
 
+function playerBWin() {
+    alert("Player B win ! ! !")
+}
+
+function testVictory() {
+    if (casePlayerA.indexOf(1) > -1 && casePlayerA.indexOf(2) > -1 && casePlayerA.indexOf(3) > -1) {
+        playerAWin();
+    };
+    if (casePlayerA.indexOf(4) > -1 && casePlayerA.indexOf(5) > -1 && casePlayerA.indexOf(6) > -1) {
+        playerAWin();
+    };
+    if (casePlayerA.indexOf(7) > -1 && casePlayerA.indexOf(8) > -1 && casePlayerA.indexOf(9) > -1) {
+        playerAWin();
+    };
+    if (casePlayerA.indexOf(1) > -1 && casePlayerA.indexOf(4) > -1 && casePlayerA.indexOf(7) > -1) {
+        playerAWin();
+    };
+    if (casePlayerA.indexOf(2) > -1 && casePlayerA.indexOf(5) > -1 && casePlayerA.indexOf(8) > -1) {
+        playerAWin();
+    };
+    if (casePlayerA.indexOf(3) > -1 && casePlayerA.indexOf(6) > -1 && casePlayerA.indexOf(9) > -1) {
+        playerAWin();
+    };
+    if (casePlayerA.indexOf(1) > -1 && casePlayerA.indexOf(5) > -1 && casePlayerA.indexOf(9) > -1) {
+        playerAWin();
+    };
+    if (casePlayerA.indexOf(3) > -1 && casePlayerA.indexOf(5) > -1 && casePlayerA.indexOf(7) > -1) {
+        playerAWin();
+    };
+}
